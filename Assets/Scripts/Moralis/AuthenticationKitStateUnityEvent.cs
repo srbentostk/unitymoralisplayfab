cbb7e939dd9dd205fba548a8efc0591c5667b559
xactant <xactant@gmail.com>
2022-08-10 18:48:17 -0400

    Inital checkin

---
+++ Assets/Scripts/Moralis/AuthenticationKitStateUnityEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.Moralis
+{
+    /// <summary>
+    /// See <see cref="AuthenticationKit"/> comments for a feature overview.
+    ///
+    /// The main event for <see cref="AuthenticationKit"/>.
+    /// 
+    /// </summary>
+    [Serializable]
+    public class AuthenticationKitStateUnityEvent : UnityEvent<AuthenticationKitState>
+    {
+    }
+}
---
+++ Assets/Scripts/Moralis/Enums.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Assets.Scripts.Moralis
+{
+    /// <summary>
+    /// List of possible platforms of the <see cref="AuthenticationKit"/>.
+    /// </summary>
+    public enum AuthenticationKitPlatform
+    {
+        None,
+        Android,
+        iOS,
+        WalletConnect,
+        WebGL
+    }
+
+    /// <summary>
+    /// List of possible states of the <see cref="AuthenticationKit"/>.
+    /// </summary>
+    public enum AuthenticationKitState
+    {
+        None,
+        PreInitialized,
+        Initializing,
+        Initialized,
+        WalletConnecting,
+        WalletConnected,
+        WalletSigning,
+        WalletSigned,
+        MoralisLoggingIn,
+        MoralisLoggedIn,
+        Disconnecting,
+        Disconnected
+    }
+}
